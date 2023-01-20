c4a6a50bc0402e4f30bbe89f605477ef73fc6437
Ashu7004 <91724666+Ashu7004@users.noreply.github.com>
2023-01-20 18:07:14 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+import './App.css';
+import {useState} from 'react';
+import Welcome from './Welcome';
+
+function App() {
+    const [name , setName] = useState("")
+    const [submit , setSubmit] = useState(false)
+
+    return (
+      <div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div className='login-page'>
+        <div>
+            <label>Enter Your Name -</label>
+            &nbsp;&nbsp;
+            <input
+            placeholder ="Your name"
+            value ={name}
+            onChange={(event)=>{setName(event.target.value)}}></input>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <button onClick={()=>{setSubmit(!submit)}}>Submit</button>
+            </div> 
+            
+            {(submit && name) && <Welcome name={name}></Welcome>}
+
+            <div>
+              <p>Assignment done by Ashutosh Kumar 12016432 </p>
+            </div>
+        </div>
+      </div>
+
+        
+    );
+
+}
+
+export default App;
